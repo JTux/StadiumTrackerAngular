c013d9303ff4df2789db2e5c4e100e432e3bdebd
Joshua Tucker <thejltucker@gmail.com>
2019-06-26 20:36:04 -0400

    Added multiple visitor selectors to Game Create

--- src/app/components/game-components/game-create/game-create.component.ts
+++ src/app/components/game-components/game-create/game-create.component.ts
@@ -6,6 +6,18 @@ import { TeamDetail } from 'src/app/models/TeamModels';
 import { StadiumDetail } from 'src/app/models/StadiumModels';
 import { TeamService } from 'src/app/services/team.service';
 import { StadiumService } from 'src/app/services/stadium.service';
+import { VisitorService } from 'src/app/services/visitor.service';
+import { VisitorDetail } from 'src/app/models/VisitorModels';
+
+export class VisitorCreateModel {
+  value: number;
+  disabled: boolean;
+
+  constructor(value: number) {
+    this.value = value;
+    this.disabled = false;
+  }
+}
 
 @Component({
   selector: 'app-game-create',
@@ -14,6 +26,10 @@ import { StadiumService } from 'src/app/services/stadium.service';
 })
 export class GameCreateComponent implements OnInit {
 
+  visitorCount = 0;
+  visitors: VisitorCreateModel[];
+  visNumbs: number[];
+  peopleList: VisitorDetail[];
   gameForm: FormGroup;
   teams: TeamDetail[];
   stadiums: StadiumDetail[];
@@ -21,6 +37,7 @@ export class GameCreateComponent implements OnInit {
   constructor(
     private _gameService: GameService,
     private _teamService: TeamService,
+    private _visitorService: VisitorService,
     private _stadiumService: StadiumService,
     private _form: FormBuilder,
     private _router: Router) {
@@ -32,9 +49,34 @@ export class GameCreateComponent implements OnInit {
     this._stadiumService.getStadiums().subscribe((stadiums: StadiumDetail[]) => {
       this.stadiums = stadiums;
     });
+    this._visitorService.getVisitors().subscribe((visitors: VisitorDetail[])=>{
+      this.peopleList = visitors;
+    });
+    this.visitors = new Array();
   }
 
   ngOnInit() {
+  }
+
+  addVisitorField() {
+    this.visitors.push(new VisitorCreateModel(this.visitorCount));
+    this.visitorCount += 1;
+  }
+
+  removeVisitor(visitor) {
+    let element = document.getElementById(`visitor${visitor.value}`);
+    element.parentNode.parentNode.removeChild(element.parentElement);
+
+    this.visitors.find(function (boy) {
+      return boy.value == visitor.value;
+    }).disabled = true;
+
+    this.visitors.forEach(v => {
+      if (v.disabled == true) {
+        let index = this.visitors.indexOf(v);
+        this.visitors.splice(index, index +1);
+      }
+    });
   }
 
   createForm() {
@@ -43,7 +85,8 @@ export class GameCreateComponent implements OnInit {
       StadiumID: new FormControl,
       HomeTeamID: new FormControl,
       AwayTeamID: new FormControl,
-      HomeTeamWon: new FormControl(false)
+      HomeTeamWon: new FormControl(false),
+      Visitors: new FormControl
     });
   }
 
